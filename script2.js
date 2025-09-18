053a8645427a4c8c5b1a7fcb9f8d9d041dde48b1
CCSSnawrin <ccss.nawrin@aol.com>
2025-09-18 12:28:22 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("contact-form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        alert("Thank you! Your message has been sent.");
+        form.reset();
+    });
+});
+
+  if (window.location.pathname.endsWith('/index.html')) {
+    window.location.replace('/home');
+  }
+
+
